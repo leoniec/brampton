74a78d5077bb71ff9f1c41b0316f16c22b27a112
leonie <leoniecitron@gmail.com>
2017-11-20 09:43:29 +0000

    i get it

---
+++ 1darrays.py
@@ -0,0 +1,14 @@
+#DECLARE myPetName : Array [0..3] of STRING
+#myPetName [0] = "Paddy"
+#myPetName [1] = "Hector"
+#myPetName [2] = "Jeff"
+#myPetName [3] = "Gertrude"
+#for index <-- 0 to 3
+#output myPetName [index]
+#NEXT index
+
+myPetName [0] = "Paddy"
+myPetName [1] = "Hector"
+myPetName [2] = "Jeff"
+myPetName [3] = "Gertrude"
+
