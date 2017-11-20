dcabacef0e2ab22fb1437cb7577d2ffc7cb57c3f
leonie <leoniecitron@gmail.com>
2017-11-20 09:46:23 +0000

    i get it

--- 1darrays.py
+++ 1darrays.py
@@ -6,9 +6,11 @@
 #for index <-- 0 to 3
 #output myPetName [index]
 #NEXT index
-
+myPetName = ["","","",""]
 myPetName [0] = "Paddy"
 myPetName [1] = "Hector"
 myPetName [2] = "Jeff"
 myPetName [3] = "Gertrude"
+
+print (myPetName)
 
